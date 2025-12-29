875cff400fb2bb2c4a4393cc9b2a71f136c5b7c7
rajaasim4 <rajaasim4474@gmail.com>
2025-12-29 19:58:13 +0500

    Refactor reservation components to enable client-side rendering and improve code organization
    - Converted several components to use client-side rendering by adding "use client" directive.
    - Removed commented-out code and unnecessary imports for cleaner code.
    - Updated the DailyCapacityWidget, ReservationPoliciesDialog, ReservationQueryDialog, ReservationHeader, and ReservationSuccess components to streamline functionality and enhance readability.
    - Implemented error handling and loading states in the DailyCapacityWidget.
    - Improved the structure of the ReservationSuccess page to handle payment verification and display reservation details effectively.

--- src/app/[locale]/reservation/success/print.tsx
+++ src/app/[locale]/reservation/success/print.tsx
@@ -1,19 +1,3 @@
-// "use client";
-// import React from "react";
-
-// export default function PrintButton() {
-//     return (
-//         <button
-//             onClick={() => window.print()}
-//             className="bg-blue-600 text-white text-center px-4 py-2.5 rounded-lg hover:bg-blue-700 transition-colors font-medium text-sm"
-//         >
-//             Print Confirmation
-//         </button>
-//     );
-// }
-
-//new code
-
 "use client";
 import React from "react";
 import { useTranslations } from "next-intl";
